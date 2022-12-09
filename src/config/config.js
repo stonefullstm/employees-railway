d7db10520946756533953c6f8cf6da7f69a90ed3
Reinaldo <reinaldoper83@gmail.com>
2022-12-08 21:19:01 -0300

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,19 @@
+// src/config/config.js
+
+require('dotenv').config();
+
+
+const config = {
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
