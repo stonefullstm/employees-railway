0eb367932c4a0974efe36a8f0cf5eacc10e8d8b8
Reinaldo <reinaldoper83@gmail.com>
2022-12-12 10:31:43 -0300

    ajustes do cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 // src/app.js
 
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const employee = require('./controllers/employee.controller');
 
 const app = express();
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get('/employees', employee.getAll);
 
